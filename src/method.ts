4a3d7d457b4b462cd234ea831d60c27837a1e3a6
Ronan <ronanmingon69550@gmail.com>
2023-02-20 06:56:40 +0100

    add route in class

--- src/method.ts
+++ src/method.ts
@@ -1,7 +1,10 @@
 type Method = 'GET' | 'POST' | 'PATCH' | 'DELETE' | 'PUT'
 
 export default function Method(method: Method, params?: string) {
-  return function<T extends Record<Method, any>>(target: T, key: keyof T, descriptor: PropertyDescriptor) {
-    target[method] = target[key]
+  return function<T>(target: T, key: keyof T, descriptor: PropertyDescriptor) {
+    if (!(<any>target).routes) {
+      (<any>target).routes = []
+    }
+    (<any>target).routes.push({params, key, method})
   }
 }
