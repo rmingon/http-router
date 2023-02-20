de656b7a07be37b0d2dfc9922c2c6cf0c73a7a2b
rmingon <ronanmingon69550@gmail.com>
2023-02-20 15:10:05 +0100

    add body parser

--- src/route.ts
+++ src/route.ts
@@ -1,5 +1,6 @@
 import http from "http";
 import url from "url";
+import {Req, Res} from './type'
 let routes : Record<string, any> = {}
 let instances = new Map()
 interface RouteDeclaration {
@@ -8,9 +9,31 @@ interface RouteDeclaration {
   method: string
 }
 
-http.createServer(function (req, res) {
+async function parseBody(req: http.IncomingMessage) : Promise<string> {
+  return new Promise((resolve, reject) => {
+    try {
+      let body :any = [];
+      req.on('error', (err) => {
+        console.error(err);
+      }).on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).toString()
+        resolve(JSON.parse(body))
+      });
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+http.createServer(async function (request: http.IncomingMessage , res) {
+  let req : Req = request
   const _url = url.parse(req.url ?? '')
+
   const url_with_params = req.url?.split('/').map((el: string) => parseInt(el) ? ':id' : el).join('/')
+
+  req.body = await parseBody(req)
   try {
     const {instance_name, key} = routes[url_with_params ?? ''][req.method ?? '']
     return (instances.get(instance_name))[key](req, res)
@@ -19,7 +42,6 @@ http.createServer(function (req, res) {
     res.write(JSON.stringify(e));
     return res.end()
   }
-
 }).listen(process.env.PORT ?? 8081);
 
 export default function Route(route: string) {
