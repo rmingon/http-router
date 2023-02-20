77140b71854462436b25b5afbc4ce0d52eea5301
Ronan <ronanmingon69550@gmail.com>
2023-02-20 06:57:22 +0100

    mount routes

--- src/route.ts
+++ src/route.ts
@@ -1,10 +1,21 @@
-import {routes} from "./index";
+export let routes : Record<string, any> = {}
+
+interface RouteDeclaration {
+  params?: string
+  key: string
+  method: string
+}
 
 export default function Route(route: string) {
-  return function<T extends { new (...args: any[]): {} }>(target: T) {
-    if (!routes.has(route)) {
-      let instance = new target()
-      routes.set(route, instance)
-    }
+  return function<T extends { new (...args: any[]): Record<any, any> }>(target: T) {
+    let instance = new target()
+    instance.routes.forEach((el: RouteDeclaration) => {
+      const route_with_params = route+(el.params ?? '')
+      if (!routes[route_with_params]) {
+        routes[route_with_params] = {}
+      }
+      routes[route_with_params][el.method] = (<any>instance)[el.key]
+      console.log("MOUNTED ROUTE" , route + (el.params ?? '') , "METHOD" , el.method)
+    })
   }
 }
